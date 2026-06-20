7f4209b0cc583c7dad9ad1edbb9ea51d417e522f
ilya807 <ilyariazanov854@gmail.com>
2026-06-20 19:50:53 +0300

    Update teststest_logic.py

--- logic_automation /teststest_logic.py
+++ logic_automation /teststest_logic.py
@@ -3,12 +3,12 @@ import pytest
 # ФИКСТУРА: Она "готовит" игру к тесту
 @pytest.fixture
 def player_with_sword():
-    # Представь, что это команда серверу: "Дай игроку меч прямо сейчас"
+    # это команда серверу: "Дай игроку меч прямо сейчас"
     inventory = ["Iron Sword"]
     return inventory
 
 def test_sell_sword(player_with_sword):
-    """Тест продажи: Нам не важно, как меч появился, мы просто его продаем"""
+    """ Нам не важно, как меч появился, мы просто его продаем"""
     inventory = player_with_sword
     item_to_sell = "Iron Sword"
     
